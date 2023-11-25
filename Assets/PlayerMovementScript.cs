abba87c2dc8d76f660ba7bbdef3295925f1d78de
blakelauper <Lauperbw@gmail.com>
2023-11-25 14:30:02 -0500

    Added player movement and jump
    Ensured VS works and also added basic movement.

--- Assets/PlayerMovementScript.cs
+++ Assets/PlayerMovementScript.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour
 {
+
+    Rigidbody2D rb;
+    float MOVE_SPEED = 6.0f;
+    float JUMP_FORCE = 7.5f;
+    float xDirection = 0.0f;
+    bool isGrounded = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        xDirection = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(xDirection * MOVE_SPEED, rb.velocity.y);
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, JUMP_FORCE);
+        }
+
+        if (xDirection > 0.0f)
+        {
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (xDirection < 0.0f)
+        {
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
 }
