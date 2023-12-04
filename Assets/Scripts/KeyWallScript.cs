6ced7315e2666ce9b8c637db3168016690914468
blakelauper <Lauperbw@gmail.com>
2023-12-04 12:09:50 -0500

    Wall fixed

--- Assets/Scripts/KeyWallScript.cs
+++ Assets/Scripts/KeyWallScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class KeyWallScript : MonoBehaviour
@@ -7,15 +8,37 @@ public class KeyWallScript : MonoBehaviour
     [SerializeField]
     GameState gameState;
 
+    // Keep track of the number of players touching the object
+    private int playersTouching = 0;
+    private void Start()
+    {
+        gameState.keyCount = 5;
+    }
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Check if player
-        if (collision.gameObject.CompareTag("Player1") && collision.gameObject.CompareTag("Player2"))
+        // Check if the collision is with a player
+        if (collision.gameObject.CompareTag("Player1") || collision.gameObject.CompareTag("Player2"))
         {
-            if (gameState.keyCount == gameState.keysNeeded)
+            // Increment the count of players touching the object
+            playersTouching++;
+
+            // Check if both players are touching the object
+            if (playersTouching >= 2 && gameState.keyCount >= gameState.keysNeeded)
             {
+                // Destroy the object if both players are touching and key count is sufficient
                 Destroy(gameObject);
             }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // Check if a player is leaving the object
+        if (collision.gameObject.CompareTag("Player1") || collision.gameObject.CompareTag("Player2"))
+        {
+            // Decrement the count of players touching the object
+            playersTouching = Mathf.Max(0, playersTouching - 1);
         }
     }
 }
